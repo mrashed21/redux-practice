4baf6c7b976c19fecee18fdc364b9fd754c6f530
mrashed21 <rashedjaman768@gmail.com>
2025-05-19 17:08:30 +0600

    add slice and provider

---
+++ providers/Provider.tsx
@@ -0,0 +1,9 @@
+import store from "@/app/redux/store/store";
+import { ReactNode } from "react";
+import { Provider } from "react-redux";
+
+const Providers = ({ children }: { children: ReactNode }) => {
+  return <Provider store={store}>{children}</Provider>;
+};
+
+export default Providers;
